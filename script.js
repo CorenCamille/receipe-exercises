96416be30ad2ccc16a6a6c2b2ed6caeb0b5f4c7f
Camille <66014059+CorenCamille@users.noreply.github.com>
2020-08-18 20:04:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var oneFourth = "1/4";
+var two = 2;
+
+function marinaraSauce(cookName) {
+	console.log("Hey, " + cookName + "! This is my marinara sauce recipe!");
+	console.log("- Heat a medium-large saucepan over medium heat");
+	console.log("- Add " + two + " tspns of oil and 5 carlic cloves, cook until golden, about " + two + " minutes");
+	console.log("- Add " + oneFourth + " cup water, " + two + " cans of crushed tomates, and salt and season with black pepper to taste.");
+	console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+	console.log("- Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+marinaraSauce("Adam");
+
+};
+
+var total = 10;
+total = total + 10;
+
+console.log(total);
+
